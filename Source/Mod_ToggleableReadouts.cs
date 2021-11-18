24e52c2ceff7b96c5e485858833ab5f46da99d48
Owlchemist <owlchemist@protonmail.com>
2021-11-17 16:55:57 -0800

    Initial release version

--- Source/Mod_ToggleableReadouts.cs
+++ Source/Mod_ToggleableReadouts.cs
@@ -6,6 +6,7 @@ using System;
 using System.Linq;
 using RimWorld;
 using static ToggleableReadouts.ToggleableReadoutsUtility;
+using static ToggleableReadouts.ModSettings_ToggleableReadouts;
  
 namespace ToggleableReadouts
 {
@@ -14,74 +15,38 @@ namespace ToggleableReadouts
 		public Mod_ToggleableReadouts(ModContentPack content) : base(content)
 		{
 			new Harmony(this.Content.PackageIdPlayerFacing).PatchAll();
-			//base.GetSettings<ModSettings_ToggleableReadouts>();
+			base.GetSettings<ModSettings_ToggleableReadouts>();
 			LongEventHandler.QueueLongEvent(() => Setup(), null, false, null);
 		}
-		/*
-		static int lastNumOfLines = 1;
+		
 		public override void DoSettingsWindowContents(Rect inRect)
 		{
+			inRect.yMin += 20f;
+			inRect.yMax -= 20f;
 			Listing_Standard options = new Listing_Standard();
-			//Make stationary rect for the filter box
-			Rect filterRect = new Rect(inRect.x, inRect.y + 30f, inRect.width, 100f);
-			//Prepare scrollable view area rect
-			Rect scrollViewRect = inRect;
-			scrollViewRect.y += 30f;
-			scrollViewRect.yMax -= 30f;
-			
-			//Prepare line height cache
-			Text.Font = GameFont.Tiny;
-			TextAnchor anchor = Text.Anchor;
-			Text.Anchor = TextAnchor.MiddleLeft;
+			Rect outRect = new Rect(inRect.x, inRect.y, inRect.width, inRect.height);
+			Rect rect = new Rect(0f, 0f, inRect.width - 30f, inRect.height * 1.2f);
+			Widgets.BeginScrollView(outRect, ref scrollPos, rect, true);
+			options.Begin(rect);
 
-			//Calculate size of rect based on content
-			Rect listRect = new Rect(0f, 0f, inRect.width - 30f, (lastNumOfLines + 2) * lineHeight);
-
-			options.Begin(inRect);
-				filter = options.TextEntryLabeled("Filter: ", filter);
-				filtered = !String.IsNullOrEmpty(filter);
-			options.End();
-			Widgets.BeginScrollView(scrollViewRect, ref scrollPos, listRect, true);
-				options.Begin(listRect);
-				
-				//List out the removed defs first
-				for (int i = 0; i < workingList.Count; i++)
-				{
-					string key = workingList.ElementAt(i);
-					Def def = GetDef(key);
-					if (def != null && (!filtered || Search(def) != 0)) 
-					{
-						cellPosition += lineHeight;
-						++lineNumber;
-						if (cellPosition > scrollPos.y - inRect.height && cellPosition < scrollPos.y + inRect.height) DrawListItem(options, def);
-					}
-				}
-
-				//Gap
-				options.curY = cellPosition + lineHeight;
+				options.Label("ToggleableReadouts.Settings.Help".Translate());
 				options.GapLine();
-				cellPosition += lineHeight;
-
-				//List out all the unremoved defs from the compiled database
-				int length = allDefs.Length;
-				for (int i = 0; i < length; ++i)
+				options.Gap();
+				for (int i = 0; i < filteredDefs.Count; ++i)
 				{
-					Def def = allDefs[i];
-					if (!workingList.Contains(GetKey(def)))
+					Def def = filteredDefs.ElementAt(i);
+					string defType = "ToggleableReadouts.Settings.Thing".Translate();
+					if (def is ThingCategoryDef) defType = "ToggleableReadouts.Settings.ThingCategoryDef".Translate();
+					
+					if (def != null && options.ButtonText(def.label + " (" + defType + ")"))
 					{
-						if (def != null && (!filtered || Search(def) != 0)) 
-						{
-							cellPosition += lineHeight;
-							++lineNumber;
-							if (cellPosition > scrollPos.y - inRect.height && cellPosition < scrollPos.y + inRect.height) DrawListItem(options, def);
-						}
+						filteredDefs.Remove(def);
+						ticker = 119;
+						BuildRootCache();
+						break;
 					}
 				}
-				lastNumOfLines = lineNumber;
-				lineNumber = 0;
-				cellPosition = 0;
-				Text.Font = GameFont.Small;
-				Text.Anchor = anchor;
+
 				options.End();
 			Widgets.EndScrollView();
 			
@@ -90,33 +55,34 @@ namespace ToggleableReadouts
 
 		public override string SettingsCategory()
 		{
-			return "Cherry Picker";
+			return "Toggleable Readouts";
 		}
 
 		public override void WriteSettings()
 		{
-			ProcessList();
 			base.WriteSettings();
 		}
-		*/
+		
 	}
-
-	/*
+	
 	public class ModSettings_ToggleableReadouts : ModSettings
 	{
 		public override void ExposeData()
 		{
-			if (Scribe.mode != LoadSaveMode.Saving || legacyKeys?.Count > 0 ) Scribe_Collections.Look(ref legacyKeys, "removedDefs", LookMode.Value, LookMode.Value);
-			Scribe_Collections.Look(ref removedDefs, "keys", LookMode.Value);
-			if (removedDefs == null) removedDefs = new HashSet<string>();
+			if (Scribe.mode == LoadSaveMode.Saving)
+			{
+				exposedFilteredDefs.Clear();
+				foreach (var def in filteredDefs)
+				{
+					exposedFilteredDefs.Add((def.GetType().Name + "/" + def.defName));
+				}
+			}
 
+			Scribe_Collections.Look(ref exposedFilteredDefs, "exposedFilteredDefs", LookMode.Value);
 			base.ExposeData();
 		}
-
-		public static List<string> legacyKeys = new List<string>();
-		public static HashSet<string> removedDefs = new HashSet<string>();
-		public static Vector2 scrollPos = Vector2.zero;
-		public static string filter;
+		public static HashSet<string> exposedFilteredDefs;
+		public static HashSet<Def> filteredDefs;
+		public static Vector2 scrollPos;
 	}
-	*/
 }
--- Source/ToggleableReadoutsUtility.cs
+++ Source/ToggleableReadoutsUtility.cs
@@ -7,34 +7,55 @@ using UnityEngine;
 using Verse.Sound;
 using static ToggleableReadouts.FastGUI;
 using static ToggleableReadouts.ResourceBank;
+using static ToggleableReadouts.ModSettings_ToggleableReadouts;
  
 namespace ToggleableReadouts
 {
     internal static class ToggleableReadoutsUtility
 	{
-		static Dictionary<Def, ReadoutCache> readoutCache = new Dictionary<Def, ReadoutCache>();
+		//Primary
+		static ReadoutCache[] readoutCache;
 		public static bool updateNow = true;
+		public static int ticker = 119;
+
+		//Cache
 		public static Event eventCache;
 		public static EventType eventType;
 		static GUIContent guiContent = GUIContent.Temp("");
 		static GUIStyle guiStyle;
-		public static bool mouseOver;
-		public static int ticker = 119;
-		static Listing_ResourceReadout list;
 		static int numOfCategories;
-		static ThingCategoryDef[] rootThingCategories;
+		public static bool mouseOver;
+		static Listing_ResourceReadout list;
 
 		public static void Setup()
 		{
-			DefDatabase<ThingCategoryDef>.AllDefsListForReading.ForEach(x => readoutCache.Add(x, new ReadoutCache(null, 0, x)));
-			rootThingCategories = DefDatabase<ThingCategoryDef>.AllDefsListForReading.Where(x => x.resourceReadoutRoot).ToArray();
-			numOfCategories = rootThingCategories.Length;
+			filteredDefs = new HashSet<Def>();
+			if (exposedFilteredDefs == null) exposedFilteredDefs = new HashSet<string>();
+			else
+			{
+				foreach (var entry in exposedFilteredDefs)
+				{
+					string defName = entry.Split('/')[1];
+					string type = entry.Split('/')[0];
+
+					if (type == nameof(ThingDef)) filteredDefs.Add(DefDatabase<ThingDef>.GetNamed(defName, true));
+					else if (type == nameof(ThingCategoryDef)) filteredDefs.Add(DefDatabase<ThingCategoryDef>.GetNamed(defName, true));
+				}
+			}
+
+			BuildRootCache();
 			
 			guiStyle = Text.fontStyles[1];
 			list = new Listing_ResourceReadout(null);
 			list.nestIndentWidth = 7f;
 			list.lineHeight = 24f;
 			list.verticalSpacing = 0f;
+		}
+
+		public static void BuildRootCache()
+		{
+			readoutCache = DefDatabase<ThingCategoryDef>.AllDefsListForReading.Where(x => x.resourceReadoutRoot && !filteredDefs.Contains(x)).Select(x => new ReadoutCache(null, 0, x)).ToArray();
+			numOfCategories = readoutCache.Length;
 		}
 
 		public static void DoReadoutCategorizedFast(ResourceReadout instance, Rect rect)
@@ -54,20 +75,21 @@ namespace ToggleableReadouts
 			mouseOver = Mouse.IsOver(rect);
 			for (int i = 0; i < numOfCategories; ++i)
 			{
-				DoCategoryFast(list, rootThingCategories[i].treeNode, 0, 32);
+				DoCategoryFast(list, readoutCache[i], 0, 32);
 			}
 			
 			GUIClip.Internal_Pop();
 			instance.lastDrawnHeight = Math.Max(list.CurHeight, list.maxHeightColumnSeen);
 		}
 
-		public static void DoCategoryFast(Listing_ResourceReadout list, TreeNode_ThingCategory node, int nestLevel, int openMask)
+		public static void DoCategoryFast(Listing_ResourceReadout list, ReadoutCache readout, int nestLevel, int openMask)
 		{
+			TreeNode_ThingCategory node = ((ThingCategoryDef)readout.def).treeNode;
+
 			//Category expanded?
 			bool expanded = (node.openBits & openMask) != 0;
 
 			//Fetch or build the cache
-			ReadoutCache readout = readoutCache[node.catDef];
 			if (updateNow) readout.Update(list, nestLevel, node.catDef, expanded);			
 			if (readout.value == 0) return;
 
@@ -88,13 +110,14 @@ namespace ToggleableReadouts
 			{
 				DrawTextureFast(readout.containerRect, TexUI.HighlightTex, colorWhite);
 				TooltipHandler.TipRegion(readout.containerRect, new TipSignal(node.catDef.LabelCap, node.catDef.GetHashCode()));
+				HandleClicks(eventCache, eventType, readout.containerRect, node.catDef);
 			}
 			
 			//Draw icon
 			DrawTextureFast(readout.iconRect, node.catDef.icon, colorWhite);
 
 			//Draw label
-			guiContent.m_Text = readout.label;
+			guiContent.m_Text = readout.valueLabel;
 			guiStyle.alignment = TextAnchor.MiddleLeft;
 			guiStyle.Internal_Draw_Injected(ref readout.labelRect, guiContent, false, false, false, false);
 
@@ -105,13 +128,12 @@ namespace ToggleableReadouts
 
 		static void DoCategoryChildrenFast(ReadoutCache readout, Listing_ResourceReadout list, int indentLevel, int openMask)
 		{
-			int length = readout.categories.Length;
-			for (int i = 0; i < length; ++i)
+			for (int i = 0; i < readout.numOfCategories; ++i)
 			{
 				DoCategoryFast(list, readout.categories[i], indentLevel, openMask);
 			}
-			length = readout.things.Count;
-			for (int i = 0; i < length; i++)
+
+			for (int i = 0; i < readout.numOfThings; i++)
 			{
 				ReadoutCache entry = readout.things[i];
 				if (entry.value != 0 ) DoThingDefFast(entry, list, indentLevel + 1);
@@ -120,32 +142,114 @@ namespace ToggleableReadouts
 
 		static void DoThingDefFast(ReadoutCache readout, Listing_ResourceReadout list, int nestLevel)
 		{
-			ThingDef thingDef = readout.thingDef;
+			ThingDef thingDef = readout.def as ThingDef;
 
 			//Compensate curY
-			readout.containerRect.y = readout.highlightRect.y = readout.labelRect.y = list.curY;
-			readout.iconRect.y = list.curY + readout.containerRect.height / 2f - readout.iconRect.height / 2f;
+			readout.containerRect.m_YMin = readout.highlightRect.m_YMin = readout.labelRect.m_YMin = list.curY;
+			readout.iconRect.m_YMin = list.curY - 2;
 			
 			if (mouseOver && Mouse.IsOver(readout.containerRect))
 			{
 				DrawTextureFast(readout.containerRect, TexUI.HighlightTex, colorWhite);
 				TooltipHandler.TipRegion(readout.containerRect, new TipSignal(() => thingDef.LabelCap + ": " + thingDef.description.CapitalizeFirst(), (int)thingDef.shortHash));
+				HandleClicks(eventCache, eventType, readout.containerRect, thingDef);
 			}
 			DrawTextureFast(readout.iconRect, thingDef.uiIcon, thingDef.graphicData.color);
 
-			guiContent.m_Text = readout.label;
+			guiContent.m_Text = readout.valueLabel;
 			guiStyle.Internal_Draw_Injected(ref readout.labelRect, guiContent, false, false, false, false);
 			list.EndLine();
+		}
+
+		static void HandleClicks(Event eventCurrent, EventType eventType, Rect rect, Def def)
+		{
+			int mouseButton = eventCurrent.button;
+			if (mouseButton == 1)
+			{
+				if (eventType == EventType.MouseDown) eventCurrent.Use();
+				else if (eventType == EventType.MouseUp)
+				{
+					List<FloatMenuOption> righClickMenu = HandleRightClick(def).ToList<FloatMenuOption>();
+					if (righClickMenu.Count != 0)
+					{
+						Find.WindowStack.Add(new FloatMenu(righClickMenu));
+						eventCurrent.Use();
+					}
+				}
+			}
+		}
+
+		static IEnumerable<FloatMenuOption> HandleRightClick(Def def)
+		{	
+			//Add option to make this pawn a group leader
+			yield return new FloatMenuOption("ToggleableReadouts.Filter".Translate(def.label), delegate()
+			{
+				FilterDef(def);
+			}, MenuOptionPriority.Default, null, null, 0f, null, null, true, 0);
+
+			yield break;
+		}
+
+		static void FilterDef(Def def)
+		{
+			if (filteredDefs.Add(def))
+			{
+				Messages.Message("ToggleableReadouts.FilterApplied".Translate(def.label), MessageTypeDefOf.NeutralEvent, false);
+				if (def is ThingDef)
+				{
+					foreach (var entry in readoutCache)
+					{
+						entry.numOfThings -= entry.things.RemoveAll(x => x.def == def);
+						entry.categories?.ToList().ForEach(x => x.numOfThings -= x.things.RemoveAll(x => x.def == def));
+					}
+				}
+				else if (def is ThingCategoryDef)
+				{
+					readoutCache = readoutCache.Where(x => x.def != def).ToArray();
+					numOfCategories = readoutCache.Length;
+					foreach (var entry in readoutCache)
+					{
+						entry.categories = entry.categories?.Where(x => x.def != def).ToArray();
+					}
+				}
+				ticker = 119;
+			}
+			LoadedModManager.GetMod<Mod_ToggleableReadouts>().WriteSettings();
+		}
+
+		static int GetCountIn(ThingCategoryDef cat, Listing_ResourceReadout list)
+		{
+			int num = 0;
+			for (int i = 0; i < cat.childThingDefs.Count; ++i)
+			{
+				var def = cat.childThingDefs[i];
+				if (filteredDefs.Contains(def)) continue;
+				num += list.map.resourceCounter.GetCount(cat.childThingDefs[i]);
+			}
+			for (int j = 0; j < cat.childCategories.Count; ++j)
+			{
+				if (!cat.childCategories[j].resourceReadoutRoot)
+				{
+					var def = cat.childCategories[j];
+					if (filteredDefs.Contains(def)) continue;
+					num += GetCountIn(cat.childCategories[j], list);
+				}
+			}
+			return num;
 		}
 
 		public class ReadoutCache
 		{
 			public ReadoutCache(Listing_ResourceReadout list = null, int nestLevel = 0, Def def = null)
 			{
+				this.def = def;
 				if (list != null) Update(list, nestLevel, def);
 				if (def is ThingCategoryDef)
 				{
-					categories = ((ThingCategoryDef)def).childCategories?.Where(x => !x.treeNode?.catDef.resourceReadoutRoot ?? false)?.Select(y => y.treeNode)?.ToArray();
+					categories = ((ThingCategoryDef)def).childCategories?.Where
+						(x => (!x.treeNode?.catDef.resourceReadoutRoot ?? false) && !filteredDefs.Contains(x))?.Select
+							(y => new ReadoutCache(null, nestLevel + 1, y)).ToArray();
+					numOfCategories = categories.Length;
 					things = new List<ReadoutCache>();
 				}
 			}
@@ -158,14 +262,14 @@ namespace ToggleableReadouts
 				}
 				else if (def is ThingCategoryDef)
 				{
-					value = list.map.resourceCounter.GetCountIn(def as ThingCategoryDef);
+					value = GetCountIn(def as ThingCategoryDef, list);
 					if (value == 0) return;
 
 					buttonRect = new Rect(list.XAtIndentLevel(nestLevel), list.curY + list.lineHeight / 2f - 9f, 18f, 18f);
 					controlID = GUIUtility.GetControlID(GUI.s_ButonHash, FocusType.Passive, buttonRect);					
 				}
 				//Printed label
-				label = value.ToStringCached();
+				valueLabel = value.ToStringCached();
 				
 				//Container
 				containerRect = new Rect(0f, list.curY, list.LabelWidth, list.lineHeight) { xMin = list.XAtIndentLevel(nestLevel) + 18f };
@@ -187,31 +291,28 @@ namespace ToggleableReadouts
 				//Handle children defs
 				if (expanded)
 				{
-					var thingsTmp = ((ThingCategoryDef)def).childThingDefs?.Where(x => x.PlayerAcquirable && list.map.resourceCounter.GetCount(x) > 0);
-					foreach (var thing in thingsTmp)
+					IEnumerable<ThingDef> childDefs = ((ThingCategoryDef)def).childThingDefs?.Where
+						(x => x.PlayerAcquirable && (list.map.resourceCounter.GetCount(x) > 0 || things.Any(y => y.def == x)) && !filteredDefs.Contains(x));
+					foreach (var thing in childDefs)
 					{
-						ReadoutCache readout = things.FirstOrDefault(x => x.thingDef == thing);
+						ReadoutCache readout = things.FirstOrDefault(x => x.def == thing);
 						if (readout == null)
 						{
 							readout = new ReadoutCache(list, nestLevel + 1, thing);
-							readout.thingDef = thing;
+							readout.def = thing;
 							things.Add(readout);
 						}
 						else readout.Update(list, nestLevel + 1, thing);
+						numOfThings = things.Count;
 					}
 				}
 			}
 			public List<ReadoutCache> things;
-			public ThingDef thingDef;
-			public int value;
-			public string label;
-			public Rect containerRect;
-			public Rect highlightRect;
-			public Rect iconRect;
-			public Rect labelRect;
-			public Rect buttonRect;
-			public int controlID;
-			public TreeNode_ThingCategory[] categories;
+			public ReadoutCache[] categories;
+			public Def def;
+			public string valueLabel;
+			public int value, controlID, numOfThings, numOfCategories;
+			public Rect containerRect, highlightRect, iconRect, labelRect, buttonRect;
 		}
 	}
 }
